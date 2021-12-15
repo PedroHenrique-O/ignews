a5f848c04a73ef18b1d819b85ac084c53fe7e058
Pedro <pedrokaz.henrique@gmail.com>
2021-12-15 17:29:40 -0300

    added dinamic stripe product using staticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,56 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import Image from "next/image";
+import styles from "./homes.module.scss";
+import avatar from "../../public/images/avatar.svg";
+import { SubButton } from "../components/SubButton";
+import { stripe } from "../services/stripe";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
-        <title>Início | IG News</title>
+        <title>Home | IG News</title>
       </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>🖐 Hey, welcome</span>
+          <h1>
+            News about <br />
+            the <span>React</span> world.
+          </h1>
+          <p>
+            Get acess to all publications <br />
+            <span>for {product.amount} month</span>
+          </p>
+          <SubButton priceId={product.priceId} />
+        </section>
+        <Image src={avatar} alt="girl" />
+      </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1K6oHXEeQHH0kUvItvA9wjvl");
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24 horas
+  };
+};
