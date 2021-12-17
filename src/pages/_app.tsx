5694c320d0e959cfc905d63daec15d189029c78b
Pedro <pedrokaz.henrique@gmail.com>
2021-12-17 00:40:05 -0300

    commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/global.scss";
 import { AppProps } from "next/app";
 import { Header } from "../components/Header";
 import { SessionProvider } from "next-auth/react";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
