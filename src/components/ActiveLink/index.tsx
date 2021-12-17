ba94332404e6746628607d91ea21d8870ccbd225
Pedro <pedrokaz.henrique@gmail.com>
2021-12-17 00:33:59 -0300

    added active link component

---
+++ src/components/ActiveLink/index.tsx
@@ -0,0 +1,21 @@
+import Link, { LinkProps } from "next/link";
+import { useRouter } from "next/router";
+import { cloneElement } from "react";
+
+interface ActiveLink extends LinkProps {
+  children: React.ReactElement;
+  activeClassName: string;
+}
+
+export function ActiveLink({
+  children,
+  activeClassName,
+
+  ...rest
+}: ActiveLink) {
+  const { asPath } = useRouter();
+
+  const className = asPath === rest.href ? activeClassName : "";
+
+  return <Link {...rest}>{cloneElement(children, { className })}</Link>;
+}
