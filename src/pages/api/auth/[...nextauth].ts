8034396592322fed7fb532e99a924f538bbcc32c
Pedro <pedrokaz.henrique@gmail.com>
2021-12-17 00:39:55 -0300

    added session into fauna

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -19,9 +19,41 @@ export default NextAuth({
   //   secret: process.env.SIGNIN_KEY,
   // },
   callbacks: {
+    async session({ session }) {
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index("subscription_by_user_ref"),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index("user_by_email"),
+                      // q.Index("user_by_email"),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(q.Index("subscription_by_status"), "active"),
+            ])
+          )
+        );
+
+        console.log("Index::", userActiveSubscription);
+        return { ...session, activeSubscription: userActiveSubscription };
+      } catch (err) {
+        console.error("erro :", err);
+        return {
+          ...session,
+          activeSubscription: null,
+        };
+      }
+    },
     async signIn({ user, account, profile, credentials }) {
       const { email } = user;
-      console.log(email);
 
       try {
         await fauna.query(
